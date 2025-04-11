79d110d1fec782a15eeb23edeefea1fc0d2fe5d5
hammoh7 <soahammohaadkar@gmail.com>
2025-04-11 20:10:38 +0530

    Assignment 6

---
+++ Assgn6/LeaderElection.java
@@ -0,0 +1,10 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface LeaderElection extends Remote {
+    void startElection(int nodeId) throws RemoteException;
+
+    void announceLeader(int leaderId) throws RemoteException;
+
+    boolean ping() throws RemoteException;
+}
---
+++ Assgn6/RingElection.java
@@ -0,0 +1,6 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface RingElection extends Remote {
+    void startElection(int initiatorId, int maxId) throws RemoteException;
+}
